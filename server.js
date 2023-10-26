49dd39c24fa655472cdd7682aa1b934e36fde14c
Rona <longhairshortcode@gmail.com>
2023-10-26 16:23:50 -0700

    edit

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ app.use(express.json())
 let persons = [
     { 
         "id": 1,
-        "name": "Artos Hellas", 
+        "name": "Arto Hellas", 
         "number": "040-123456"
       },
       { 
         "id": 2,
-        "name": "Adala Lovelace", 
+        "name": "Ada Lovelace", 
         "number": "39-44-5323523"
       },
       { 
         "id": 3,
-        "name": "Daniel Abramov", 
+        "name": "Dan Abramov", 
         "number": "12-43-234345"
       },
       { 
         "id": 4,
-        "name": "Mary-Ann Popendieck", 
+        "name": "Mary Poppendieck", 
         "number": "39-23-6423122"
       },
       {
