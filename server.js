e261ae257695f940a46ee75f21a78f414f043cb5
Rona <longhairshortcode@gmail.com>
2023-10-29 18:10:51 -0700

    3.1-3.7 code edit for 10/29

--- server.js
+++ server.js
@@ -169,3 +169,8 @@ app.listen(PORT, () =>{
 app.listen(PORT, () =>{
   console.log(`The server is running on port ${PORT}`)
 })
+//delete
+//app.listen 
+app.listen(PORT, () =>{
+  console.log(`The server is running on port ${PORT}`)
+})
